52e9106672478998e61a84d7cee54e3972bca786
Animesh Pandey <apanimesh061@gmail.com>
2014-09-17 12:17:56 -0400

    Managed to generate a very basic plot of the data points and regression line.

--- lin_reg.rkt
+++ lin_reg.rkt
@@ -1,12 +1,24 @@
-#lang racket/gui
-
+#lang racket
 (require 2htdp/image)
 (require 2htdp/batch-io)
+(require 2htdp/universe)
 
-(define-struct Point (x y))
+(define newline "\n")
+
+(define scaling-x 20)
+(define scaling-y 4)
+
+(define plot-frame-width 500)
+(define plot-frame-height 500)
+
+(define (scale-var var factor)
+  (* var factor)
+  )
+
+(define-struct Point (x y) #:transparent)
 
 (define create-plot-point
-  (circle 1 "solid" "red"))
+  (circle 2 "solid" "blue"))
 
 (define (read-data-set filename)
   (read-csv-file/rows filename (lambda (x) x))
@@ -48,12 +60,12 @@
   )
 
 (define (embed-circle P backimage)
-  (place-image create-plot-point (Point-x P) (Point-y P) backimage)
+  (place-image create-plot-point (* (Point-x P) scaling-x) (* (Point-y P) scaling-y) backimage)
   )
 
 (define (embed-engine lst)
   (cond
-    [(empty? lst) (empty-scene 500 500)]
+    [(empty? lst) (empty-scene plot-frame-height plot-frame-width)]
     [else (embed-circle 
            (first lst)
            (embed-engine (rest lst)))]
@@ -78,7 +90,46 @@
        (*(-(Point-x l) (mean X-vec)) (-(Point-x l) (mean X-vec)))
        ) final-data)) (- N 1)))
 
+(define (f-of-x n)
+  (+ Alpha (* Beta n)))
+
 (define Beta (/ covariance-xy variance-x))
 (define Alpha (- Y-mean (* Beta X-mean)))
+(define line-start 
+  (make-Point 
+   (* scaling-x 0) 
+   (* scaling-y 
+    (f-of-x 0))
+   ))
 
-(flip-vertical (embed-engine final-data))
+(define line-end 
+  (make-Point 
+   (* scaling-x 25) 
+   (* scaling-y 
+    (f-of-x 25))
+   ))
+
+(define render-graph (flip-vertical
+(add-line 
+ (embed-engine final-data) 
+ (Point-x line-start) 
+ (Point-y line-start) 
+ (Point-x line-end) 
+ (Point-y line-end) 
+ "red")))
+
+(define equation (string-append 
+ "y = " 
+ (number->string Alpha) 
+ " + " 
+ (number->string Beta) 
+ "x"))
+
+render-graph
+(display newline)
+(string-append "Sample Size: " (number->string N))
+(string-append "Mean x: " (number->string X-mean))
+(string-append "Mean y: " (number->string Y-mean))
+(string-append "Intercept (Alpha): " (number->string Alpha))
+(string-append "Slope (Beta): " (number->string Beta))
+(string-append "Regression line equation: " equation)
